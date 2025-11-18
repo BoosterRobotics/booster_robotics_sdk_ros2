0b75c4c1fea1e8affa468d4ddb95a59ebd519548
xy-Meepo <xy-Meepo@booster-robotics.com>
2025-11-18 18:12:55 +0800

    feat: fix commit

--- shared-file-system/script/s-40-build-component.sh
+++ shared-file-system/script/s-40-build-component.sh
@@ -7,3 +7,4 @@ set -v
 closure_build_directory_component VAR_COL_CON_BUILD=2 VAR_COMPONENT_DIR_PATH="${CUSTOM_ENV_PROJECT_DIRECTORY_PATH}/booster_ros2_interface" VAR_COMPONENT_ROS_NAME=booster_interface || exit
 
 closure_upload_artifact
+
