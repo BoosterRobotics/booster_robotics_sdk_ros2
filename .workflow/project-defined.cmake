d6c62b443f0186985fe7a6e623544a91162304be
xy-Meepo <xy-Meepo@booster-robotics.com>
2025-11-20 18:30:12 +0800

    fix: build script 11-20

--- .workflow/project-defined.cmake
+++ .workflow/project-defined.cmake
@@ -1,7 +1,3 @@
-macro(closure_import_prepare_project)
-endmacro()
-
-
 macro(closure_import_install_project)
     if (DEFINED ARGV0 AND NOT "${ARGV0}" STREQUAL "")
         cmake_language(CALL ${ARGV0})
@@ -15,3 +11,27 @@ endmacro()
 
 macro(closure_link_target_lib i_lib_target)
 endmacro()
+
+
+macro(closure_generate_project_name)
+    get_filename_component(PLUGIN_TARGET_NAME "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
+    string(REPLACE "-" "_" C_MAKE_CUSTOM_SUB_LINE_TARGET_NAME ${PLUGIN_TARGET_NAME})
+    set(C_MAKE_CUSTOM_PROJECT_NAME "project_${C_MAKE_CUSTOM_SUB_LINE_TARGET_NAME}")
+endmacro()
+
+
+
+
+
+
+
+
+
+
+
+
+macro(closure_import_prepare_project)
+endmacro()
+
+closure_generate_project_name()
+closure_import_prepare_project()
